43f07764ca286f9f805884125672e73cab82335f
Jose Vigil <josemanuelvigil@gmail.com>
2025-02-09 13:07:52 -0300

    Enhance uninstallation process: - Added removal of /Users/josevigil/Documents/mybash in uninstall.sh. - Included deletion of additional symbolic links (/usr/local/bin/mypy). - Improved user prompts for deleting mybash directory and data directory. - Ensured safe removal of 'source /mybash.zsh' from ~/.zshrc.

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,13 @@
 
 # Variables
 MYBASH_DIR="$HOME/mybash"
+MYBASH_DATA_DIR="$HOME/Documents/mybash"
 
 # Function to remove symbolic links
 remove_symlinks() {
     echo "Removing symbolic links from /usr/local/bin..."
     rm -f /usr/local/bin/mybash
+    rm -f /usr/local/bin/mypy
 }
 
 # Function to remove mybash.zsh from ~/.zshrc
@@ -32,10 +34,23 @@ delete_mybash_dir() {
     fi
 }
 
+# Function to delete the mybash data directory
+delete_mybash_data_dir() {
+    read -p "Do you want to delete the mybash data directory ($MYBASH_DATA_DIR)? (y/n): " choice
+    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+        echo "Deleting $MYBASH_DATA_DIR..."
+        rm -rf "$MYBASH_DATA_DIR"
+        MYBASH_DATA_DELETED=true
+    else
+        echo "Skipping deletion of $MYBASH_DATA_DIR."
+    fi
+}
+
 # Main logic
 remove_symlinks
 remove_from_zshrc
 delete_mybash_dir
+delete_mybash_data_dir
 
 # Reload shell configuration only if the mybash directory was not deleted
 if [[ -z "$MYBASH_DELETED" ]]; then
